97645c625afa9ef006d60441c33a36e5b0aa9368
Shannon Lewis <slewis74@gmail.com>
2016-11-15 10:32:46 +1000

    removing the idea of absolute paths, paths should be relative, or in the whitelist

--- source/AuthenticationExtensibilityTests/IntegratedAuthenticationRedirectUrlTests.cs
+++ source/AuthenticationExtensibilityTests/IntegratedAuthenticationRedirectUrlTests.cs
@@ -7,20 +7,19 @@ namespace AuthenticationExtensibilityTests
     [TestFixture]
     public class IntegratedAuthenticationRedirectUrlTests
     {
-        [TestCase("http://site1", "/folder1/api", null, ExpectedResult = true)]
-        [TestCase("http://site1", "~/folder1/api", null, ExpectedResult = true)]
-        [TestCase("http://site1", "http://site1/folder1/api", null, ExpectedResult = true)]
-        [TestCase("http://localhost:8065", "http://localhost:9005", new [] { "http://localhost:9005" }, ExpectedResult = true)]
-        [TestCase("http://localhost:8065", "https://localhost:9005", new[] { "https://localhost:9005" }, ExpectedResult = true)]
-        [TestCase("https://localhost:8065", "https://localhost:9005", new[] { "https://localhost:9005" }, ExpectedResult = true)]
-        [TestCase("https://localhost:8065", "http://localhost:9005", new[] { "http://localhost:9005" }, ExpectedResult = true)]
-        [TestCase("http://site1", "folder1/api", null, ExpectedResult = false)]
-        [TestCase("http://site1", "http://site2/folder1/api", null, ExpectedResult = false)]
-        [TestCase("http://site1", "http://site2/folder1/api", new[] {""}, ExpectedResult = false)]
-        [TestCase("https://localhost:8065", "http://localhost:9005", new[] { "https://localhost:9005" }, ExpectedResult = false)]
-        public bool IsPathLocalTest(string directoryPath, string url, string[] whitelist)
+        [TestCase("/folder1/api", null, ExpectedResult = true)]
+        [TestCase("~/folder1/api", null, ExpectedResult = true)]
+        [TestCase("http://site1/folder1/api", null, ExpectedResult = false)]
+        [TestCase("http://localhost:9005", new [] { "http://localhost:9005" }, ExpectedResult = true)]
+        [TestCase("https://localhost:9005", new[] { "https://localhost:9005" }, ExpectedResult = true)]
+        [TestCase("https://localhost:9005", new[] { "https://localhost:9005" }, ExpectedResult = true)]
+        [TestCase("http://localhost:9005", new[] { "http://localhost:9005" }, ExpectedResult = true)]
+        [TestCase("folder1/api", null, ExpectedResult = false)]
+        [TestCase("http://site2/folder1/api", new[] {""}, ExpectedResult = false)]
+        [TestCase("http://localhost:9005", new[] { "https://localhost:9005" }, ExpectedResult = false)]
+        public bool IsPathLocalTest(string url, string[] whitelist)
         {
-            return Requests.IsLocalUrl(directoryPath, url, whitelist);
+            return Requests.IsLocalUrl(url, whitelist);
         }
     }
 }
--- source/Octopus.Server.Extensibility.Authentication/HostServices/Requests.cs
+++ source/Octopus.Server.Extensibility.Authentication/HostServices/Requests.cs
@@ -5,18 +5,15 @@ namespace Octopus.Server.Extensibility.Authentication.HostServices
 {
     public static class Requests
     {
-        public static bool IsLocalUrl(string absoluteVirtualDirectoryPath, string url, string[] whitelist)
+        public static bool IsLocalUrl(string url, string[] whitelist)
         {
             // Credit to Microsoft - Preventing Open Redirection Attacks (C#)
             // http://www.asp.net/mvc/overview/security/preventing-open-redirection-attacks
 
             var isLocalUrl = !string.IsNullOrEmpty(url) &&
 
-                             // Allows full paths that start with the absoluteVirtualDirectoryPath
-                             (url.StartsWith(absoluteVirtualDirectoryPath) ||
-
                              // Allows paths on the corsWhitelist, if one is defined
-                             (whitelist != null && whitelist.Length > 0 && whitelist.Where(u => !string.IsNullOrWhiteSpace(u)).Any(url.StartsWith)) ||
+                             ((whitelist != null && whitelist.Length > 0 && whitelist.Where(u => !string.IsNullOrWhiteSpace(u)).Any(url.StartsWith)) ||
 
                              // Allows "/" or "/foo" but not "//" or "/\".
                              (url[0] == '/' && (url.Length == 1 || (url[1] != '/' && url[1] != '\\'))) ||
