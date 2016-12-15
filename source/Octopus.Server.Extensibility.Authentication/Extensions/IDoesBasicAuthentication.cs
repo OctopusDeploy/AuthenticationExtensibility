457e1a46639407d0c5da13a189e717496dc8a08c
Shannon Lewis <slewis74@gmail.com>
2016-12-16 07:12:48 +1000

    new interface for BasicAuthentication

---
+++ source/Octopus.Server.Extensibility.Authentication/Extensions/IDoesBasicAuthentication.cs
@@ -0,0 +1,9 @@
+﻿using Octopus.Data.Storage.User;
+
+namespace Octopus.Server.Extensibility.Authentication.Extensions
+{
+    public interface IDoesBasicAuthentication
+    {
+        UserCreateOrUpdateResult ValidateCredentials(string username, string password);
+    }
+}
