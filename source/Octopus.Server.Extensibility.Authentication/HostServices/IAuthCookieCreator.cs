c83d040b52eec6d48171d7e67ad38080ef38f95c
Michael Noonan <michaelnoonan@gmail.com>
2017-02-07 08:37:26 +1000

    Change IAuthCookieCreator interface to handle multiple cookies.
    In this case the driving force is CSRF prevention using the Cookie-To-Token pattern.
    
    +semver: breaking

--- source/Octopus.Server.Extensibility.Authentication/HostServices/IAuthCookieCreator.cs
+++ source/Octopus.Server.Extensibility.Authentication/HostServices/IAuthCookieCreator.cs
@@ -6,6 +6,12 @@ namespace Octopus.Server.Extensibility.Authentication.HostServices
 {
     public interface IAuthCookieCreator
     {
+        [ObsoleteEx(
+            Message = "Sorry about the immediately breaking change! We are introducing CSRF prevention using the Cookie-To-Token pattern. Calling this method will only build the authentication cookie. Please change to use the CreateAuthCookies() method which will create the correct cookies for you to add on the outbound Response object.",
+            RemoveInVersion = "3.1.0",
+            TreatAsErrorFromVersion = "3.0.0",
+            ReplacementTypeOrMember = "CreateAuthCookies")]
         INancyCookie CreateAuthCookie(NancyContext context, Guid token, bool persistant);
+        INancyCookie[] CreateAuthCookies(NancyContext context, Guid token, SessionExpiry expiry);
     }
 }
