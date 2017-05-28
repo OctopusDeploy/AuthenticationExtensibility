59df4eb58b9df67f879275c3fba96bb50c6b4e0d
Nick Josevski <nick@josevski.com>
2017-05-28 22:43:01 +1000

    split to .core which is netstandard, and ODCM.Auth

---
+++ source/ODCM.Server.Extensibility.Authentication/HostServices/IAuthCookieCreator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ODCM.Server.Extensibility.Authentication.HostServices
+{
+    public interface IAuthCookieCreator
+    {
+        // TODO: work out if it will be this interface, which will be confusing
+        // or if new one IJwtCreator IJwtSomething
+    }
+}
