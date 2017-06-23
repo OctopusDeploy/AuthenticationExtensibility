7c729b7ec50c62f0b4d696be39c0dbfb8e87ca5e
Shannon Lewis <slewis74@gmail.com>
2017-06-23 15:00:23 +1000

    allow identity update, which AD will want to do

--- source/Node.Extensibility.Authentication/HostServices/IUpdateableUserStore.cs
+++ source/Node.Extensibility.Authentication/HostServices/IUpdateableUserStore.cs
@@ -21,6 +21,7 @@ namespace Octopus.Node.Extensibility.Authentication.HostServices
         void DisableUser(string userId);
 
         IUser AddIdentity(string userId, Identity identity);
+        IUser UpdateIdentity(string userId, Identity identity);
         
         void ClearSecurityGroupIds(string provider, string userId);
         void SetSecurityGroupIds(string provider, string userId, IEnumerable<string> ids, DateTimeOffset updated);
