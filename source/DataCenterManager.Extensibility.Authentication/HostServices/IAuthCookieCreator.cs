8c4b3abbdab8a90ad2083efcda0161fb029f6499
Shannon Lewis <slewis74@gmail.com>
2017-07-07 11:17:06 +1000

    adding IAuthCookieCreator for aspnet core

---
+++ source/DataCenterManager.Extensibility.Authentication/HostServices/IAuthCookieCreator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Octopus.Node.Extensibility.Authentication.HostServices;
+
+namespace Octopus.DataCenterManager.Extensibility.Authentication.HostServices
+{
+    public interface IAuthCookieCreator
+    {
+        void CreateAuthCookies(HttpResponse response, Guid token, SessionExpiry expiry);
+    }
+}
