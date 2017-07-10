cf25f6abef749f9d51b652d0f6158a47a50731ee
Shannon Lewis <slewis74@gmail.com>
2017-07-10 15:22:09 +1000

    added IJwtCreator

---
+++ source/DataCenterManager.Extensibility.Authentication/HostServices/IJwtCreator.cs
@@ -0,0 +1,9 @@
+﻿using Octopus.Data.Model.User;
+
+namespace Octopus.DataCenterManager.Extensibility.Authentication.HostServices
+{
+    public interface IJwtCreator
+    {
+        string CreateFor(IUser user);
+    }
+}
