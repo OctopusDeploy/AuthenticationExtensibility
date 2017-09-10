10e6116a10ea7c4d2417ddbcc114f9200fa103b8
Shannon Lewis <slewis74@gmail.com>
2017-09-10 18:02:31 +1000

    CancellationToken got dropped in merge

--- source/Node.Extensibility.Authentication/Extensions/ISupportsAutoUserCreationFromPrincipal.cs
+++ source/Node.Extensibility.Authentication/Extensions/ISupportsAutoUserCreationFromPrincipal.cs
@@ -1,10 +1,11 @@
 ﻿using System.Security.Principal;
+using System.Threading;
 using Octopus.Node.Extensibility.Authentication.Storage.User;
 
 namespace Octopus.Node.Extensibility.Authentication.Extensions
 {
     public interface ISupportsAutoUserCreationFromPrincipal
     {
-        AuthenticationUserCreateResult GetOrCreateUser(IPrincipal principal);
+        AuthenticationUserCreateResult GetOrCreateUser(IPrincipal principal, CancellationToken cancellationToken);
     }
 }
