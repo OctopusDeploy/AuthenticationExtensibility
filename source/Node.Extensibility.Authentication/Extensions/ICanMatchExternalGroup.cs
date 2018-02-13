7b8aca0f39cf42b0465b12a73dda1441edcbd2ef
Shannon Lewis <slewis74@gmail.com>
2018-02-13 10:12:10 +1000

    doco update to help make it clearer what to expect back from the function

--- source/Node.Extensibility.Authentication/Extensions/ICanMatchExternalGroup.cs
+++ source/Node.Extensibility.Authentication/Extensions/ICanMatchExternalGroup.cs
@@ -8,7 +8,7 @@ namespace Octopus.Node.Extensibility.Authentication.Extensions
         /// <summary>
         /// Returns an identity if one can be exactly matched to the given name
         /// </summary>
-        /// <returns>The matching identity or null if no match could be found.</returns>
+        /// <returns>Returns null if the provider is disabled, otherwise a result with a matching ExternalSecurityGroup if a single match was found. If none or more than 1 were found the group will be null.</returns>
         ExternalSecurityGroupResult Match(string name, CancellationToken cancellationToken);
     }
 }
