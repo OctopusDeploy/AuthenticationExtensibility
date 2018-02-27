0458d381a18af8af016e3b6918a0dd2023aa34a4
Shannon Lewis <slewis74@gmail.com>
2018-02-27 13:56:33 +1000

    renamed some interfaces to make things more consistent. Removed the idea of matching groups, you search for groups and the caller gets to worry about whether 0, 1 or more is what they expect.

--- source/Node.Extensibility.Authentication/Extensions/ExternalSecurityGroupResult.cs
+++ source/Node.Extensibility.Authentication/Extensions/ExternalSecurityGroupResult.cs
@@ -4,7 +4,13 @@ namespace Octopus.Node.Extensibility.Authentication.Extensions
 {
     public class ExternalSecurityGroupResult
     {
+        public ExternalSecurityGroupResult(string providerame, ExternalSecurityGroup[] groups)
+        {
+            Providerame = providerame;
+            Groups = groups;
+        }
+
         public string Providerame { get; set; }
-        public ExternalSecurityGroup Group { get; set; }
+        public ExternalSecurityGroup[] Groups { get; set; }
     }
 }
