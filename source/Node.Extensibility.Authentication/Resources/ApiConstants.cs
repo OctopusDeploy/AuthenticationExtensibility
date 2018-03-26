c030b686bffe094555da233e8db73e61dd925959
Shannon Lewis <slewis74@gmail.com>
2018-03-26 20:04:58 +1000

    rethink on the structures. Using a LoginState object to represent the desired post login state info

--- source/Node.Extensibility.Authentication/Resources/ApiConstants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Octopus.Node.Extensibility.Authentication.Resources
-{
-    public class ApiConstants
-    {
-        public static readonly string OctopusRequestAbsoluteUrlHeaderName = "X-Octopus-Request-Absolute-Url";
-    }
-}
