0cbf51c9e54dc60b7072f61eb2a8f172e3a43582
Shannon Lewis <slewis74@gmail.com>
2018-10-04 21:26:44 +1000

    added interface for extensions to indicate whether an external SSO server has initiated a login to Octopus

---
+++ source/Node.Extensibility.Authentication/Extensions/ICanHandleLoginParameters.cs
@@ -0,0 +1,9 @@
+﻿using Octopus.CoreUtilities;
+
+namespace Octopus.Node.Extensibility.Authentication.Extensions
+{
+    public interface ICanHandleLoginParameters
+    {
+        Maybe<LoginInitiatedResult> WasExternalLoginInitiated(string encodedQueryString);
+    }
+}
