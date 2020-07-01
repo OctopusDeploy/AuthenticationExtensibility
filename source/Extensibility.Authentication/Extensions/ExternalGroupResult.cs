1fa4e8eb9dc2006657a3645e105b1d72f2c814c4
Shannon Lewis <slewis74@gmail.com>
2020-07-01 10:29:00 +1000

    remove the package reference to CoreUtilities and a class property that has been replaced with something else

--- source/Extensibility.Authentication/Extensions/ExternalGroupResult.cs
+++ source/Extensibility.Authentication/Extensions/ExternalGroupResult.cs
@@ -4,13 +4,10 @@ namespace Octopus.Server.Extensibility.Authentication.Extensions
 {
     public class ExternalGroupResult
     {
-        public ExternalGroupResult(string identityProviderName, IEnumerable<string> groupIds)
+        public ExternalGroupResult(IEnumerable<string> groupIds)
         {
-            IdentityProviderName = identityProviderName;
             GroupIds = groupIds;
         }
-
-        public string IdentityProviderName { get; }
 
         public IEnumerable<string>? GroupIds { get; }
     }
